cfe7c431cf11e8780b5a77abdfa464244c4adb77
Charles Raj <charles.raj412@gmail.com>
2024-06-04 18:45:34 -0400

    Initial Commit

---
+++ RealmDemo/RealmDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmDemoApp.swift
+//  RealmDemo
+//
+//  Created by charles raj on 04/06/24.
+//
+
+import SwiftUI
+
+@main
+struct RealmDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
